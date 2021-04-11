822057f74a2c3da2e643978032a57668011debe5
Ron Martin <cpuwzd@comcast.net>
2021-04-10 21:09:39 -0400

    Check for duplicate cache ID's.
    Issue a new error message each time a new duplicate cache ID is found. Each message refers to the original occurrence of the cache ID. Comparisons are case sensitive and culture insensitive. Cache ID's created from hashes are included in the process.
    
    Fixes wixtoolset/issues#4628

--- src/WixToolset.Core.Burn/Bundles/ProcessExePackageCommand.cs
+++ src/WixToolset.Core.Burn/Bundles/ProcessExePackageCommand.cs
@@ -4,22 +4,27 @@ namespace WixToolset.Core.Burn.Bundles
 {
     using System;
     using System.Collections.Generic;
+    using WixToolset.Data;
     using WixToolset.Data.Symbols;
+    using WixToolset.Extensibility.Services;
 
     /// <summary>
     /// Initializes package state from the Exe contents.
     /// </summary>
     internal class ProcessExePackageCommand
     {
-        public ProcessExePackageCommand(PackageFacade facade, Dictionary<string, WixBundlePayloadSymbol> payloadSymbols)
+        public ProcessExePackageCommand(IMessaging messaging, PackageFacade facade, Dictionary<string, WixBundlePayloadSymbol> payloadSymbols, Dictionary<string, SourceLineNumber> duplicateCacheIdDetector)
         {
+            this.Messaging = messaging;
             this.AuthoredPayloads = payloadSymbols;
             this.Facade = facade;
+            this.DuplicateCacheIdDetector = duplicateCacheIdDetector;
         }
 
-        public Dictionary<string, WixBundlePayloadSymbol> AuthoredPayloads { get; }
-
+        public IMessaging Messaging { get; }
         public PackageFacade Facade { get; }
+        public Dictionary<string, WixBundlePayloadSymbol> AuthoredPayloads { get; }
+        public Dictionary<string, SourceLineNumber> DuplicateCacheIdDetector { get; }
 
         /// <summary>
         /// Processes the Exe packages to add properties and payloads from the Exe packages.
@@ -31,6 +36,16 @@ namespace WixToolset.Core.Burn.Bundles
             if (String.IsNullOrEmpty(this.Facade.PackageSymbol.CacheId))
             {
                 this.Facade.PackageSymbol.CacheId = packagePayload.Hash;
+            }
+
+            if (this.DuplicateCacheIdDetector.TryGetValue(this.Facade.PackageSymbol.CacheId, out var sourceLineNumber))
+            {
+                this.Messaging.Write(ErrorMessages.DuplicateCacheIds1(sourceLineNumber, this.Facade.PackageSymbol.CacheId));
+                this.Messaging.Write(ErrorMessages.DuplicateCacheIds2(this.Facade.PackageSymbol.SourceLineNumbers, this.Facade.PackageSymbol.CacheId));
+            }
+            else
+            {
+                this.DuplicateCacheIdDetector.Add(this.Facade.PackageSymbol.CacheId, this.Facade.PackageSymbol.SourceLineNumbers);
             }
 
             this.Facade.PackageSymbol.Version = packagePayload.Version;
